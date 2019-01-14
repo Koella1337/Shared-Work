e8e4a189fc107cbd7047caf98a41c11e0d60c9d3
GeraldOppender <gerald.oppeneder@gmx.at>
2019-01-14 21:29:31 +0100

    Implemented Pathfinding for AGV System

--- Software_Engineering/src/main/java/factory/subsystems/agv/AgvCoordinator.java
+++ Software_Engineering/src/main/java/factory/subsystems/agv/AgvCoordinator.java
@@ -1,17 +1,22 @@
 package factory.subsystems.agv;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import app.gui.SubsystemMenu;
 import factory.shared.AbstractSubsystem;
 import factory.shared.Position;
+import factory.shared.ResourceBox;
 import factory.shared.Task;
 import factory.shared.Utils;
 import factory.shared.enums.SubsystemStatus;
@@ -25,6 +30,7 @@ public class AgvCoordinator extends AbstractSubsystem implements AgvMonitorInter
 	private SubsystemStatus status = SubsystemStatus.WAITING;
 	private boolean ready = false;
 	private List<AgvTask> tasks = new LinkedList<>();
+	private Pathfinder pathfinder;
 	
 	public AgvCoordinator(MonitoringInterface mon, Element factory)
 	{
@@ -43,6 +49,15 @@ public class AgvCoordinator extends AbstractSubsystem implements AgvMonitorInter
 			Forklift f = new Forklift(p, this);
 			addForklift(f);
 		}
+		try {
+			pathfinder = new Pathfinder(factory);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// BAD THINGS HAVE HAPPENED WHILE I READ THE XML FILE
+			System.out.println("AGV PATHFINDER DID BAD THINGS TO THE XML FILE");
+			e.printStackTrace();
+		}
+		
+		submitTask(new AgvTask(null, new ResourceBox(new Position(20, 20)), new ResourceBox(new Position(500, 500))));
 	}
 	
 	public void addForklift(Forklift forklift)
@@ -52,8 +67,20 @@ public class AgvCoordinator extends AbstractSubsystem implements AgvMonitorInter
 
 	@Override
 	public void submitTask(AgvTask task) {
-		System.out.println("SUBMIT AGV TASK");
-		// TODO: Choose closest free Forklift and call forklift.assignTask
+		Forklift free = null;
+		for(Forklift f : forklifts)
+		{
+			if(f.getCurrentTask() == null)
+			{
+				free = f;
+				break;
+			}
+		}
+		// calculate the Path
+		free.setPath(pathfinder.getPath(free.getPosition(), task.getPickup().getPosition()));
+		free.path.addAll(pathfinder.getPath(task.getPickup().getPosition(), task.getDropoff().getPosition()));
+		free.assignTask(task);
+		
 	}
 
 	@Override
@@ -86,7 +113,7 @@ public class AgvCoordinator extends AbstractSubsystem implements AgvMonitorInter
 	public void start() {
 		for(Forklift f : forklifts)
 		{
-			f.shutdown();
+			f.resume();
 		}
 	}
 
@@ -94,7 +121,7 @@ public class AgvCoordinator extends AbstractSubsystem implements AgvMonitorInter
 	public void stop() {
 		for(Forklift f : forklifts)
 		{
-			f.resume();
+			f.shutdown();
 		}
 	}
 
--- Software_Engineering/src/main/java/factory/subsystems/agv/Forklift.java
+++ Software_Engineering/src/main/java/factory/subsystems/agv/Forklift.java
@@ -21,8 +21,8 @@ public class Forklift implements Placeable {
 	private long lastTime; // last time the forklift's position was updated
 	private boolean shutdown;
 
-	private List<Position> path;
-	private Position vec = new Position(-1, 0); // this is for drawing, so it mustn't be null even at the start
+	public List<Position> path;
+	private Position vec = new Position(0, -1); // this is for drawing, so it mustn't be null even at the start
 
     private static Image forkliftImage;
     
@@ -46,7 +46,7 @@ public class Forklift implements Placeable {
 
 		lastTime = System.nanoTime();
 		Timer scheduler = new Timer();
-		scheduler.scheduleAtFixedRate(move, 500l, 500l); // update every half-second
+		scheduler.scheduleAtFixedRate(move, 50l, 50l); // update every half-second
 		
 		forkliftImage = new ImageIcon("resources/Forklift-v3.png").getImage();
 	}
@@ -63,6 +63,7 @@ public class Forklift implements Placeable {
 			// so I just won't assign to busy forklifts, who cares
 			// the monitor doesn't have to know
 			currentTask = newTask;
+			resume();
 		}
 		else
 		{
@@ -111,10 +112,13 @@ public class Forklift implements Placeable {
 				Double moveLen = timeElapsed / SPEED;
 				// don't overshoot the target
 				moveLen = Math.min(moveLen, len);
-				// Normalise the vector and set it to the appropriate length
-				vec = Position.divide(Position.multiply(vec, (int)Math.round(moveLen)), (int)Math.round(len));
-				// add it to our position to obtain the new one
-				pos = Position.addPosition(pos,vec);
+				if(len > 0)
+				{
+					// Normalise the vector and set it to the appropriate length
+					vec = Position.divide(Position.multiply(vec, (int)Math.round(moveLen)), (int)Math.round(len));
+					// add it to our position to obtain the new one
+					pos = Position.addPosition(pos,vec);
+				}
 
 				// when we reach a target, remove it
 				// due to floating point numbers we use a small range
